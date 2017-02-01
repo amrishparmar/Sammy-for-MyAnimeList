ce9141b7de478c6f96ca6144e161c88538fe769e
Amrish Parmar <aparm001@gold.ac.uk>
2017-02-01 15:51:10 +0000

    Added mapping dictionaries to menus.py to enable transitions between menus

--- menus.py
+++ menus.py
@@ -1,28 +1,36 @@
 import click
+import sys
+
 
 def main_menu():
-    click.echo("=== MAL CLI Application ===")
+    click.clear()
+    click.echo("===== MAL CLI Application =====")
     click.echo("1) Search for an anime")
     click.echo("2) Search for a manga")
     click.echo("3) Update your anime list")
     click.echo("4) Update your manga list")
-    click.echo("5) Exit application")
+    click.echo("0) Exit application")
     choice = click.prompt("Please choose an option", type=int)
-    return choice
+    _mm_mapping[choice]()
 
 
 def update_anime_menu():
+    click.clear()
+    click.echo("===== MAL CLI Application =====")
+    click.echo("--- Update your anime list ---")
     click.echo("1) Add new anime")
     click.echo("2) Quick increment episode count for existing anime")
     click.echo("3) Set episode count for existing anime")
     click.echo("4) Set score for existing anime")
     click.echo("5) Set status for existing anime")
-    click.echo("6) Go back to main menu")
+    click.echo("9) Go back to main menu")
     choice = click.prompt("Please choose an option", type=int)
-    return choice
+    _update_anime_mapping[choice]()
 
 
 def update_manga_menu():
+    click.clear()
+    click.echo("===== MAL CLI Application =====")
     click.echo("1) Add new manga")
     click.echo("2) Quick increment chapter count for existing manga")
     click.echo("3) Quick increment volume count for existing manga")
@@ -30,9 +38,39 @@ def update_manga_menu():
     click.echo("5) Set volume count for existing manga")
     click.echo("6) Set score for existing manga")
     click.echo("7) Set status for existing manga")
-    click.echo("8) Go back to main menu")
+    click.echo("9) Go back to main menu")
     choice = click.prompt("Please choose an option", type=int)
-    return choice
+    _update_manga_mapping[choice]()
+
+
+_mm_mapping = {
+    1: None,
+    2: None,
+    3: update_anime_menu,
+    4: update_manga_menu,
+    0: sys.exit
+}
+
+_update_anime_mapping = {
+    1: None,
+    2: None,
+    3: None,
+    4: None,
+    5: None,
+    9: main_menu,
+}
+
+_update_manga_mapping = {
+    1: None,
+    2: None,
+    3: None,
+    4: None,
+    5: None,
+    6: None,
+    7: None,
+    9: main_menu,
+}
+
 
 if __name__ == "__main__":
     main_menu()
