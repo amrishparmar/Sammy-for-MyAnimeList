8be8a3458d134b5ac94b60406e26f097e86ec782
Amrish Parmar <aparm001@gold.ac.uk>
2017-02-07 15:51:42 +0000

    Added file for mapping MAL int values to strings

---
+++ constants.py
@@ -0,0 +1,34 @@
+anime_status = {
+    1: "Watching",
+    2: "Completed",
+    3: "On hold",
+    4: "Dropped",
+    6: "Plan to watch"
+}
+
+manga_status = {
+    1: "Reading",
+    2: "Completed",
+    3: "On hold",
+    4: "Dropped",
+    6: "Plan to read"
+}
+
+anime_type = {
+    1: "TV",
+    2: "OVA",
+    3: "Movie",
+    4: "Special",
+    5: "ONA",
+    6: "Music"
+}
+
+manga_type = {
+    1: "Manga",
+    2: "Novel",
+    3: "One-shot",
+    4: "Doujinshi",
+    5: "Manhwa",
+    6: "Manhua",
+    7: "OEL"
+}
