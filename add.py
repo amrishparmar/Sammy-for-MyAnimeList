b1d10ee0f99d9f4b88f0d091fb1f0d909754f5de
Amrish Parmar <aparm001@gold.ac.uk>
2017-03-07 23:44:40 +0000

    Added comments and docstrings to new code

--- add.py
+++ add.py
@@ -5,10 +5,18 @@ import helpers
 
 
 def add_entry(credentials, entry_type, entry=None):
+    """Add a new entry to the user's anime or manga
+
+    :param credentials: A tuple containing valid MAL account details in the format (username, password)
+    :param entry_type: A string, must be either "anime" or "manga"
+    :param entry: A beautiful soup tag, an entry to add
+    :return: None, if the user cancelled
+    """
 
     if entry_type not in ["anime", "manga"]:
         raise ValueError("Invalid argument for {}, must be either {} or {}.".format(entry_type, "anime", "manga"))
 
+    # search for the entry to add if one wasn't passed in
     while entry is None:
         click.echo("Search for the entry you wish to add")
         entry = search.search(credentials, entry_type)
@@ -19,34 +27,44 @@ def add_entry(credentials, entry_type, entry=None):
         if not click.confirm("Try searching again?"):
             return
 
-    status = helpers.get_status_choice_from_user(entry_type)
-
     xml_tag_format = "<{0}>{1}</{0}>"
     xml_field_tags = ""
 
+    # get the choice of status
+    status = helpers.get_status_choice_from_user(entry_type)
+
     if status != 6:
+        # append the status tag to our xml string if the user didn't opt to skip
         xml_field_tags += xml_tag_format.format("status", status)
 
         if entry_type == "anime":
+            # get the choice of episode count
             episodes = helpers.get_new_count_from_user("episode", int(entry.episodes.get_text()))
+            # append
             if episodes is not None:
                 xml_field_tags += xml_tag_format.format("episode", episodes)
         else:
+            # get the choice of chapter and volume count
             chapters = helpers.get_new_count_from_user("chapter", int(entry.chapters.get_text()))
             volumes = helpers.get_new_count_from_user("volume", int(entry.volumes.get_text()))
 
+            # append chapter and volume choice if the user didn't opt to skip
             if chapters is not None:
                 xml_field_tags += xml_tag_format.format("chapter", chapters)
             if volumes is not None:
                 xml_field_tags += xml_tag_format.format("volume", volumes)
 
+    # get the choice of score
     score = helpers.get_score_choice_from_user()
 
+    # append score choice if the user didn't opt to skip
     if score is not None:
         xml_field_tags += xml_tag_format.format("score", score)
 
+    # form the xml string
     xml = '<?xml version="1.0" encoding="UTF-8"?><entry>{}</entry>'.format(xml_field_tags)
 
+    # make the request to the server to add
     r = requests.get("https://myanimelist.net/api/{}list/add/{}.xml".format(entry_type, entry.id.get_text()),
                      params={"data": xml}, auth=credentials)
 
@@ -60,8 +78,18 @@ def add_entry(credentials, entry_type, entry=None):
 
 
 def add_anime_entry(credentials, entry=None):
+    """Add a new anime entry to the user's anime list
+
+    :param credentials: A tuple containing valid MAL account details in the format (username, password)
+    :param entry: A beautiful soup tag, an entry to add
+    """
     add_entry(credentials, "anime", entry)
 
 
 def add_manga_entry(credentials, entry=None):
+    """Add a new manga entry to the user's manga list
+
+    :param credentials: A tuple containing valid MAL account details in the format (username, password)
+    :param entry: A beautiful soup tag, an entry to add
+    """
     add_entry(credentials, "manga", entry)
--- helpers.py
+++ helpers.py
@@ -2,10 +2,16 @@ import click
 import constants
 
 
-def get_status_choice_from_user(media_type, none_option=None):
+def get_status_choice_from_user(media_type):
+    """Prompt the user for a choice of new status and return it
+
+    :param media_type: A string, the type of media for which the status is being updated, must be "anime" or "manga"
+    :return: An integer, the new status or None if the user cancelled
+    """
     if media_type not in ["anime", "manga"]:
         raise ValueError("Invalid argument for {}, must be either {} or {}.".format(media_type, "anime", "manga"))
 
+    # select the appropriate map
     status_map = constants.ANIME_STATUS_MAP if media_type == "anime" else constants.MANGA_STATUS_MAP
 
     click.echo("Which of the following statuses do you want to update to?")
@@ -17,15 +23,17 @@ def get_status_choice_from_user(media_type, none_option=None):
     # get the number corresponding to the last option
     last_option = int(list(status_map.keys())[-1])
 
-    click.echo("{}> [{}]".format(last_option, none_option or "Cancel"))
+    # print out the option to choose none of the statuses
+    click.echo("{}> [{}]".format(last_option, "Skip"))
 
     # get a valid choice from the user
     while True:
         status = click.prompt("Choose an option", type=int)
 
+        # if the user chose a valid status
         if 0 < status < last_option:
-            return status + 1 if status == 5 else status
-        # if user chose to cancel
+            return status + 1 if status == 5 else status  # remap choice of 5 to 6 if chosen
+        # if the user cancelled
         elif status == last_option:
             return
         else:
@@ -33,12 +41,17 @@ def get_status_choice_from_user(media_type, none_option=None):
 
 
 def get_score_choice_from_user():
+    """Prompt the user for a new score and return it
+
+    :return: An integer, the new score or None if the user cancelled
+    """
     while True:
         score = click.prompt("Enter the new score (Enter -1 to cancel)", type=int)
 
         # ensure that the score is an int between 1 and 10 (inclusive)
         if 0 < score < 11:
             return score
+        # if the user cancelled
         elif score == -1:
             return
         else:
@@ -46,6 +59,12 @@ def get_score_choice_from_user():
 
 
 def get_new_count_from_user(field_type, limit=0):
+    """Prompt the user for a new episode/chapter/volume count and return it
+
+    :param field_type: A string,
+    :param limit: An integer, the number of episodes/chapters/volumes in a series
+    :return: An integer, the new episode count or None if the user cancelled
+    """
     if limit < 0:
         raise ValueError("Limit must be greater than or equal to 0")
 
@@ -53,8 +72,10 @@ def get_new_count_from_user(field_type, limit=0):
         count = click.prompt("Enter the new {} count (Enter -1 to cancel)".format(field_type), type=int)
 
         if 0 <= count:
+            # if there is no limit or there is a valid count
             if limit == 0 or count <= limit:
                 return count
+        # if the user cancelled
         elif count == -1:
             return
 
