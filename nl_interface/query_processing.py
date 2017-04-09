ad4729c2954b1efff1d0980bad23bc08fec34e9e
amrishparmar <aparm001@gold.ac.uk>
2017-04-09 15:08:08 +0100

    Added a function to determine intended action

--- nl_interface/query_processing.py
+++ nl_interface/query_processing.py
@@ -28,6 +28,14 @@ class MediaType(Enum):
     ANIME = 0
     MANGA = 1
 
+string_to_operation_map = {
+    "search": OperationType.SEARCH,
+    "update": OperationType.UPDATE,
+    "increment": OperationType.UPDATE_INCREMENT,
+    "add": OperationType.ADD,
+    "delete": OperationType.DELETE
+}
+
 
 def normalise(query):
     """Normalise a query for consistent string analysis
@@ -69,6 +77,34 @@ def strip_type(term):
     return term, None
 
 
+def determine_action(query):
+    action_term_orders = []
+
+    for action in synonyms.actions.keys():
+        for syn in synonyms.actions[action]:
+            try:
+                index = query.index(syn)
+                action_term_orders.append((action, index))
+                break
+            except ValueError:
+                continue
+
+    if not action_term_orders:
+        # the query did not match any of our known keywords
+        return None
+    else:
+        action_term_orders = sorted(action_term_orders, key=lambda x: (x[1], x[0]))
+        if len(action_term_orders) >= 2:
+            if action_term_orders[0][1] == action_term_orders[1][1]:
+                if action_term_orders[0][0] == "delete" and action_term_orders[1][0] == "search":
+                    return string_to_operation_map["delete"]
+
+                if action_term_orders[0][0] == "increment" and action_term_orders[1][0] == "update":
+                    return string_to_operation_map["update"]
+
+        return string_to_operation_map[action_term_orders[0][0]]
+
+
 def process(query):
     """Process the user query and return a dictionary with the result
 
@@ -89,94 +125,103 @@ def process(query):
             result["extra"] = "Hello, {}!"
             break
 
-    # rules for search requests
+    # determine the likely type of action the user intended
+    action = determine_action(query)
 
-    search_syns = "|".join(synonyms.actions["search"])
-    info_syns = "|".join(synonyms.terms["information"])
+    if action == OperationType.SEARCH:
+        # rules for search requests
 
-    sm1 = re.match(".*(?:{}) (?:(?:for|on) (?:the )?)?(?:{}) (?:(?:for|on) (?:the )?)?(.+ ?)+".format(
-                    search_syns, info_syns), query)
-    sm2 = re.match(".*(?:{}) (?:(?:for|on) )?(?:the )?(.+ ?)+".format(search_syns + info_syns), query)
-    sm3 = re.match(".*(?:{}) (.+)".format(search_syns), query)
+        search_syns = "|".join(synonyms.actions["search"])
+        info_syns = "|".join(synonyms.terms["information"])
 
-    if sm1 or sm2 or sm3:
-        result["operation"] = OperationType.SEARCH
-        search_terms = ""
-        if sm1:
-            search_terms = sm1.group(1)
-            print("sm1")
-        elif sm2:
-            search_terms = strip_info(sm2.group(1))
-            print("sm2")
-        elif sm3:
-            search_terms = strip_info(sm3.group(1))
-            print("sm3")
+        sm1 = re.match(".*(?:{}) (?:(?:for|on) (?:the )?)?(?:{}) (?:(?:for|on) (?:the )?)?(.+ ?)+".format(
+                        search_syns, info_syns), query)
+        sm2 = re.match(".*(?:{}) (?:(?:for|on) )?(?:the )?(.+ ?)+".format(search_syns + info_syns), query)
+        sm3 = re.match(".*(?:{}) (.+)".format(search_syns), query)
 
-        search_terms_stripped = search_terms.strip(" '\"")
-        search_terms_stripped_tuple = strip_type(search_terms_stripped)
+        if sm1 or sm2 or sm3:
+            result["operation"] = OperationType.SEARCH
+            search_terms = ""
+            if sm1:
+                search_terms = sm1.group(1)
+                print("sm1")
+            elif sm2:
+                search_terms = strip_info(sm2.group(1))
+                print("sm2")
+            elif sm3:
+                search_terms = strip_info(sm3.group(1))
+                print("sm3")
 
-        if search_terms_stripped_tuple[1] is not None:
-            result["type"] = MediaType.MANGA if search_terms_stripped_tuple[1] == "manga" else MediaType.ANIME
+            search_terms_stripped = search_terms.strip(" '\"")
+            search_terms_stripped_tuple = strip_type(search_terms_stripped)
 
-        result["term"] = search_terms_stripped_tuple[0]
+            if search_terms_stripped_tuple[1] is not None:
+                result["type"] = MediaType.MANGA if search_terms_stripped_tuple[1] == "manga" else MediaType.ANIME
 
-    # rules for add requests
+            result["term"] = search_terms_stripped_tuple[0]
 
-    add_syns = "|".join(synonyms.actions["add"])
+    elif action == OperationType.ADD:
+        # rules for add requests
 
-    am1 = re.match(".*(?:{}) (.+?)(?= (?:to )?(?:my )?(anime|manga)? ?list)".format(add_syns), query)
-    am2 = re.match(".*(?:{}) (.+)".format(add_syns), query)
+        add_syns = "|".join(synonyms.actions["add"])
 
-    if am1 or am2:
-        result["operation"] = OperationType.ADD
-        if am1:
-            print("am1")
-            add_term = am1.group(1)
-            result["type"] = MediaType.MANGA if am1.group(2) == "manga" else MediaType.ANIME
-        elif am2:
-            print("am2")
-            add_term = am2.group(1)
+        am1 = re.match(".*(?:{}) (.+?)(?= (?:to )?(?:my )?(anime|manga)? ?list)".format(add_syns), query)
+        am2 = re.match(".*(?:{}) (.+)".format(add_syns), query)
 
-        result["term"] = add_term.strip(" '\"")
+        add_term = ""
 
-    # rules for delete requests
+        if am1 or am2:
+            result["operation"] = OperationType.ADD
+            if am1:
+                print("am1")
+                add_term = am1.group(1)
+                result["type"] = MediaType.MANGA if am1.group(2) == "manga" else MediaType.ANIME
+            elif am2:
+                print("am2")
+                add_term = am2.group(1)
 
-    delete_syns = "|".join(synonyms.actions["delete"])
+            result["term"] = add_term.strip(" '\"")
 
-    dm1 = re.match(".*(?:{}) (.+?)(?: (?:off )?(?:from )?(?:my )?(anime|manga)? ?(?:list))".format(delete_syns), query)
-    dm2 = re.match(".*(?:{}) (.+)".format(delete_syns), query)
+    elif action == OperationType.DELETE:
+        delete_syns = "|".join(synonyms.actions["delete"])
 
-    if dm1 or dm2:
-        result["operation"] = OperationType.DELETE
-        if dm1:
-            print("dm1")
-            delete_term = dm1.group(1)
-            result["type"] = MediaType.MANGA if dm1.group(2) == "manga" else MediaType.ANIME
-        elif dm2:
-            print("dm2")
-            delete_term = dm2.group(1)
+        dm1 = re.match(".*(?:{}) (.+?)(?: (?:off )?(?:from )?(?:my )?(anime|manga)? ?(?:list))".format(delete_syns), query)
+        dm2 = re.match(".*(?:{}) (.+)".format(delete_syns), query)
 
-        result["term"] = delete_term.strip(" '\"")
+        delete_term = ""
 
-    increment_syns = "|".join(synonyms.actions["increment"])
+        if dm1 or dm2:
+            result["operation"] = OperationType.DELETE
+            if dm1:
+                print("dm1")
+                delete_term = dm1.group(1)
+                result["type"] = MediaType.MANGA if dm1.group(2) == "manga" else MediaType.ANIME
+            elif dm2:
+                print("dm2")
+                delete_term = dm2.group(1)
 
-    inc1 = re.match(".*(?:{}) (?:the )?(?:(episode|chapter|volume)s? )?(?:count )?(?:for |on )?(.+ ?)+?".format(
-                    increment_syns), query)
+            result["term"] = delete_term.strip(" '\"")
 
-    if inc1:
-        result["operation"] = OperationType.UPDATE_INCREMENT
-        result["type"] = MediaType.MANGA
-        increment_term = inc1.group(2)
+    elif action == OperationType.UPDATE or action == OperationType.UPDATE_INCREMENT:
+        increment_syns = "|".join(synonyms.actions["increment"])
 
-        if inc1.group(1) == "chapter":
-            modifier_term = UpdateModifier.CHAPTER
-        elif inc1.group(1) == "volume":
-            modifier_term = UpdateModifier.VOLUME
-        else:
-            modifier_term = UpdateModifier.EPISODE
-            result["type"] = MediaType.ANIME
+        inc1 = re.match(".*(?:{}) (?:the )?(?:(episode|chapter|volume)s? )?(?:count )?(?:for |on )?(.+ ?)+?".format(
+                            increment_syns), query)
 
-        result["modifier"] = modifier_term
-        result["term"] = increment_term
+        if inc1:
+            result["operation"] = OperationType.UPDATE_INCREMENT
+            result["type"] = MediaType.MANGA
+            increment_term = inc1.group(2)
+
+            if inc1.group(1) == "chapter":
+                modifier_term = UpdateModifier.CHAPTER
+            elif inc1.group(1) == "volume":
+                modifier_term = UpdateModifier.VOLUME
+            else:
+                modifier_term = UpdateModifier.EPISODE
+                result["type"] = MediaType.ANIME
+
+            result["modifier"] = modifier_term
+            result["term"] = increment_term
 
     return result
